c40335f36810fba7d359f6745c365f72d889627f
mabus <m.buscaroli@outlook.com>
2022-07-09 18:24:40 +0100

    setup debug mode and environment in Pipfile, add templates folder and basic index.html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
-    
+    user = {'username': 'John'}
+    posts = [
+        {
+            'author': {'username': 'George'},
+            'body': 'Hi there I am George!'
+        },
+        {
+            'author': {'username': 'Clare'},
+            'body': 'Hi there I am Clare!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
