d46597b5a25d6e55c5acabe3b0a478a69fe50916
mabus <m.buscaroli@outlook.com>
2022-07-09 22:46:15 +0100

    add form validation

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 
 from flask import Flask
 from config import Config
+# from flask_cors import CORS
 
 app = Flask(__name__)
+# CORS(app)
 
 # config file with secret key saved within a class called Config
 app.config.from_object(Config)
