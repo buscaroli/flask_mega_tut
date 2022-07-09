e26786f38fb272b2ffb39d3ba02b07d12fe61718
mabus <m.buscaroli@outlook.com>
2022-07-09 19:54:11 +0100

    add flask_wtf and create a login view

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
 
+# config file with secret key saved within a class called Config
+app.config.from_object(Config)
+
 # importing at the bottom as a workaround to circular imports
+# from app import routes should be here
 from app import routes
