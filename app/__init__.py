71af46037ae4cd250cde8ee3c4d228ed4a34eb7a
mabus <m.buscaroli@outlook.com>
2022-07-10 12:01:45 +0100

    add logout functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 
-from flask import Flask
+from flask import Flask, current_app
 from config import Config
 # from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # CORS(app)
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 # importing at the bottom as a workaround to circular imports
 # from app import routes should be here
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -20,10 +22,25 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if a user navigates to the /login endpoint but is authenticated
+    # refer him back to the homepage
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
-    # if validate_on_submit == True the submit btn has npot been pressed
-    # which means it is a GET request and the login form should be shown
+   
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+            #  login_user will ser the current_user var to the user for the duration 
+            #  of the session
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
